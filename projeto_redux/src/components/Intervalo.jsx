21d795df6d3069e1d5a1e496d3baf22883f4aaf1
douglas-floriano <doudouglasglascosta@gmail.com>
2022-04-02 13:56:21 +0000

    Projeto Redux Pronto

--- projeto_redux/src/components/Intervalo.jsx
+++ projeto_redux/src/components/Intervalo.jsx
@@ -3,25 +3,40 @@ import React from "react"
 import Card from "./Card"
 
 import { connect } from "react-redux"
+import { alterarNumeroMinimo } from "../store/actions/numeros"
+import { alterarNumeroMaximo } from "../store/actions/numeros"
 
 function Intervalo(props) {
-    const {min, max} = props
-    return(
-        <Card title = "Intervalo de Números" red>
+    const { min, max } = props
+    return (
+        <Card title="Intervalo de Números" red>
             <div className="Intervalo">
                 <span>
                     <span>Mínimo:</span>
                     {/* <input type="number" value={min} onChange={e => props.onMinChanged(+e.target.value)} /> */}
-                    <input type="number" value={min}/>
+                    <input type="number" value={min} onChange={e => props.alterarMinimo(+e.target.value)} />
                 </span>
                 <span>
                     <span>Máximo:</span>
                     {/* <input type="number" value={max} onChange={e => props.onMaxChanged(+e.target.value)} /> */}
-                    <input type="number" value={max}/>
+                    <input type="number" value={max} onChange={e => props.alterarMaximo(+e.target.value)} />
                 </span>
             </div>
         </Card>
     )
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        alterarMinimo(novoNumero) {
+            const action = alterarNumeroMinimo(novoNumero)
+            dispatch(action)
+        },
+        alterarMaximo(novoNumero) {
+            const action = alterarNumeroMaximo(novoNumero)
+            dispatch(action)
+        }
+    }
 }
 
 function mapStateToProps(state) {
@@ -31,4 +46,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps)(Intervalo)
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo)
